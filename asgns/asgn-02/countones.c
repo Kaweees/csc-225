8c7faad757af891dd4cf8ee79839a1b1bbac526b
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-04-14 12:39:00 -0700

    Update countones.c

--- asgns/asgn-02/countones.c
+++ asgns/asgn-02/countones.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 
 int countBits(int num);
 
 /* This is a comment */
 int main(void) {
   int num;
-  char prompt[] = "";
+  char prompt = 'y';
+  int count;
+  unsigned int cast;
   printf("Welcome to the CountOnes program.\n");
 
-  while (!(strcmp(prompt, "n")) == 0) {
+  while (prompt != 'n') {
     printf("\nPlease enter a number: ");
     scanf("%d", &num);
-    printf("The number of bits set is: %d\n", countBits(num));
+    count = 0;
+    cast = (unsigned int)num; /*cast to unsigned int*/
+    while (cast != 0) {
+      count += cast & 1; /**add lsb to counter*/
+      cast >>= 1;        /** right-shift by one position to discard lsb*/
+    }
+    printf("The number of bits set is: %d\n", count);
     printf("Continue (y/n)?: ");
-    scanf("%s", prompt);
+    scanf(" %c", &prompt);
   }
   printf("Exiting");
   return 0;
-}
-
-int countBits(int num) {
-  int count = 0;
-  unsigned int cast = (unsigned int)num; /*cast to unsigned int*/
-  while (cast != 0) {
-    count += cast & 1; /**add lsb to counter*/
-    cast >>= 1;        /** right-shift by one position to discard lsb*/
-  }
-  return count;
 }
