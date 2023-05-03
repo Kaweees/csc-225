d3fa1e31351170b8316d3fdb96d52f1fcf9999fb
Kaweees <Kaweees@users.noreply.github.com>
2023-05-03 22:38:45 +0000

    Code formatting and linting

--- asgns/asgn-04/c-code/calculator.c
+++ asgns/asgn-04/c-code/calculator.c
@@ -1,5 +1,6 @@
-#include "operations.h"
 #include <stdio.h>
+
+#include "operations.h"
 
 int main(void) {
   char prompt = 'y';
@@ -9,8 +10,9 @@ int main(void) {
   int num2;
   unsigned int cast;
   printf("Welcome to the Calculator program.\n");
-  printf("Operations - 1:add 2:subtract 3:multiply 4:divide 5:and 6:or 7:xor "
-         "8:lshift 9:rshift\n");
+  printf(
+      "Operations - 1:add 2:subtract 3:multiply 4:divide 5:and 6:or 7:xor "
+      "8:lshift 9:rshift\n");
 
   while (prompt != 'n') {
     printf("\nNumber of operations performed: %d", count);
@@ -21,36 +23,36 @@ int main(void) {
     printf("\nSelect operation: ");
     scanf("%d", &operation);
     switch (operation) {
-    case 1:
-      printf("Result: %d\n", addnums(num1, num2));
-      break;
-    case 2:
-      printf("Result: %d\n", subnums(num1, num2));
-      break;
-    case 3:
-      printf("Result: %d\n", multnums(num1, num2));
-      break;
-    case 4:
-      printf("Result: %d\n", divnums(num1, num2));
-      break;
-    case 5:
-      printf("Result: %d\n", andnums(num1, num2));
-      break;
-    case 6:
-      printf("Result: %d\n", ornums(num1, num2));
-      break;
-    case 7:
-      printf("Result: %d\n", xornums(num1, num2));
-      break;
-    case 8:
-      printf("Result: %d\n", lshiftnums(num1, num2));
-      break;
-    case 9:
-      printf("Result: %d\n", rshiftnums(num1, num2));
-      break;
-    default:
-      printf("Result: Invalid Operation\n");
-      break;
+      case 1:
+        printf("Result: %d\n", addnums(num1, num2));
+        break;
+      case 2:
+        printf("Result: %d\n", subnums(num1, num2));
+        break;
+      case 3:
+        printf("Result: %d\n", multnums(num1, num2));
+        break;
+      case 4:
+        printf("Result: %d\n", divnums(num1, num2));
+        break;
+      case 5:
+        printf("Result: %d\n", andnums(num1, num2));
+        break;
+      case 6:
+        printf("Result: %d\n", ornums(num1, num2));
+        break;
+      case 7:
+        printf("Result: %d\n", xornums(num1, num2));
+        break;
+      case 8:
+        printf("Result: %d\n", lshiftnums(num1, num2));
+        break;
+      case 9:
+        printf("Result: %d\n", rshiftnums(num1, num2));
+        break;
+      default:
+        printf("Result: Invalid Operation\n");
+        break;
     }
     printf("Continue (y/n)?: ");
     scanf(" %c", &prompt);
