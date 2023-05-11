9b2e5a42a13e913d37891de97ffceaa83658a86e
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-05-11 16:42:13 -0700

    rars

--- rars.sh
+++ rars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# chmod u+x rars.sh
 # run rars
-cd resources
 xrandr --output eDP-1 --mode 1368x768
 /usr/lib/jvm/default-java/bin/java -jar rars1_6.jar
 xrandr --output eDP-1 --mode 3840x2400
-xrandr --output eDP-1 --scale 1.0x1.0
-cd ..
+xrandr --output DP-1 --mode 2560x1080
+xrandr --output DP-1 --scale 2.25x2.25
