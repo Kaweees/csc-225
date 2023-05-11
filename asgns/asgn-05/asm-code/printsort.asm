8cbd4775840841de792f0f1b55f74ac04aad3073
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-05-10 22:56:29 -0700

    Update printsort.asm

--- asgns/asgn-05/asm-code/printsort.asm
+++ asgns/asgn-05/asm-code/printsort.asm
@@ -1,4 +1,4 @@
-  .global main, printsort, getStringLen, return_address # declare global symbols
+  .global main, getStringLen, return_address # declare global symbols
   
   .data # start of the data section
 return_address: .word -1 # return address for when subroutines are called
@@ -42,56 +42,48 @@ main: # entry point for the program
   la a0, word_alpha # load address of word_alpha into a0
   jal printstring # print word_alpha
   
-  jal printsort # print my_string
-
-endprintsort:
-  la a0, new_line # load address of new_line into a0
-  jal printstring # print my_string
-  j main
-  
-printsort:
-  li t1, 0 # i = 0
-  li t2, 0 # j = 0
-  li t5, 0 # minCeiling = 0
-  li t5, -1 # minCeiling = -1
+  li s2, 0 # i = 0
+  li s3, 0 # j = 0
+  li s4, 0 # minCeiling = 0
+  li s4, -1 # minCeiling = -1
   
 outer_loop:
-  bgt t1, s1, end_outer_loop # while i < length
-  li t3, 0 # smallCount = 0
-  li t4, 0 # smallest = 0
-  li t4, 999 # smallest = 999
-  li t2, 0 # j = 0
+  bgt s2, s1, end_outer_loop # while i < length
+  li s5, 0 # smallCount = 0
+  li s6, 0 # smallest = 0
+  li s6, 999 # smallest = 999
+  li s3, 0 # j = 0
 inner_loop:
-  bge t2, s1, print_nums # while j < length
-  add t0, s0, t2 # incremented address (j)
-  lb t0, 0(t0) # load byte at address t0 (string[j])
+  bge s3, s1, print_nums # while j < length
+  add s7, s0, s3 # incremented address (j)
+  lb s7, 0(s7) # load byte at address s7 (string[j])
   j if # goto if
 if:
-  bge t5, t0, elif # if minCeiling >= string[j], goto elif
-  bge t0, t4,elif # if string[j] >= smallest, goto elif
-  li, t3, 1 # smallCount = 0
-  mv t4, t0 # smallest = string[j]
+  bge s4, s7, elif # if minCeiling >= string[j], goto elif
+  bge s7, s6,elif # if string[j] >= smallest, goto elif
+  li, s5, 1 # smallCount = 0
+  mv s6, s7 # smallest = string[j]
   j end_if # goto end_if
 elif:
   # works if (smallest == word[j])
-  bne t0, t4, end_if # if smallest != word[j], goto end_if
-  addi t3, t3, 1 # smallCount++
+  bne s7, s6, end_if # if smallest != word[j], goto end_if
+  addi s5, s5, 1 # smallCount++
   j end_if # goto end_if
 end_if:
-  addi t2, t2, 1 # j++
+  addi s3, s3, 1 # j++
   j inner_loop # goto inner_loop
 print_nums:
-  li t2, 0 # j = 0
-  mv t5, t4 # minCeiling = smallest
+  li s3, 0 # j = 0
+  mv s4, s6 # minCeiling = smallest
 print_loop:
-  bge t2, t3, end_inner_loop # while j < smallCount
-  mv a0, t5 # a0 = smallest
+  bge s3, s5, end_inner_loop # while j < smallCount
+  mv a0, s4 # a0 = smallest
   jal printchar # print smallest
-  addi t2, t2, 1 # j++
+  addi s3, s3, 1 # j++
   j print_loop # goto print_loop
   
 end_inner_loop:
-  addi t1, t1, 1 # i++
+  addi s2, s2, 1 # i++
   j outer_loop # goto outer_loop
   
 end_outer_loop:
@@ -107,20 +99,25 @@ exit:
   # Subroutine to get the length of a given string
 # Inputs: a0 - the address of the string
 getStringLen:
-  la t0, return_len
-  sw ra, 0(t0) # save the return address
+  la s7, return_len
+  sw ra, 0(s7) # save the return address
 
-  mv t0, a0 # t0 = a0
-  li t1, 0 # length = 0
+  mv s7, a0 # s7 = a0
+  li s2, 0 # length = 0
 while:
-  lb t2, 0(t0) # t2 = string[i]
-  beqz t2, end_while # if string[i] == 0, goto end_while
-  addi t0, t0, 1 # t0 + = 1
-  addi t1, t1, 1 # i + = 1
+  lb s3, 0(s7) # s3 = string[i]
+  beqz s3, end_while # if string[i] == 0, goto end_while
+  addi s7, s7, 1 # s7 + = 1
+  addi s2, s2, 1 # i + = 1
   j while # goto while
 end_while:
-  mv a0, t1 # a0 = length
-  la t0, return_len
-  lw ra, 0(t0) # restore the return address
+  mv a0, s2 # a0 = length
+  la s7, return_len
+  lw ra, 0(s7) # restore the return address
   jr ra # return to address called from + 4
   # end subroutine
+
+endprintsort:
+  la a0, new_line # load address of new_line into a0
+  jal printstring # print my_string
+  j main
